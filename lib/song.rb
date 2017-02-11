8a2980feb02f15d7db8449dc49a782cbfbe80afc
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-11 15:58:35 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if @@all.include?(self.name)
+    if self.all.include?(self.name)
       self.find_by_name(name)
     else
       self.create_by_name(name)
