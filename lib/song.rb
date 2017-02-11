9a667bf4548afd8acb52ab50f92e921131f8f369
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-11 00:44:21 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,61 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(self.name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort do |x, y|
+      x.name <=> y.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    artist_name = filename.split(" - ")[0]
+    mp3_name = filename.split(" - ")[1]
+    name = mp3_name.split(".")[0]
+    song = self.new_by_name(name)
+    song.artist_name = artist_name
+    return song
+  end
+
+  def self.create_from_filename(filename)
+    artist_name = filename.split(" - ")[0]
+    mp3_name = filename.split(" - ")[1]
+    name = mp3_name.split(".")[0]
+    song = self.create_by_name(name)
+    song.artist_name = artist_name
+    return song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
